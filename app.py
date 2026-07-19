0e9d0e6afbf378c90b841f12c352acc55a6d3832
Ciro <ciromoraismedeiros@gmail.com>
2026-07-19 09:28:39 -0300

    Inicilizar app flask e criar index.html

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+from flask import Flask, render_template, request, redirect, url_for
 
+app = Flask(__name__)
+app.secret_key = 'super-secret-key'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
