fb0382b796521ee4c02be3b970cc8df8b6b19f44
Ciro <ciromoraismedeiros@gmail.com>
2026-07-19 10:04:03 -0300

    Criar estrutura básica de página com dados estáticos

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ app.secret_key = 'super-secret-key'
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/conteudo/<id>')
+def conteudo(id):
+    valores = { 'id': id }
+    valores['titulo'] = 'Funções'
+    valores['materiais'] = [
+        { 'titulo': 'Slides da Aula', 'url': 'exemplo.com/jsahdfkjsdhf' },
+        { 'titulo': 'Videoaula', 'url': 'youtube.com/jsahdfkjsdhf' }
+    ]
+    return render_template('conteudo.html', **valores)
