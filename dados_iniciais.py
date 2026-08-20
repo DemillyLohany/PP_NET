736c675ed47efcc4e0c7dff4be7ec7a98552c114
Filipe <fmarcelino154@gmail.com>
2026-08-19 22:35:42 -0300

    Co-authored-by: Camila <CamiPevensie@users.noreply.github.com>

--- dados_iniciais.py
+++ dados_iniciais.py
@@ -1,27 +1,39 @@
 #aqui os dados iniciais que estão inseridos no banquinho
 from sqlmodel import Session
 from database import engine
-from models import Disciplina,                                                                                                                                                                                                                                  X 
+from models import Disciplina,Conteudo,Material                                                                                                                                                                                       
 from dados_estaticos import DISCIPLINAS, TODOS_OS_CONTEUDOS
 
-#with Session(engine) as session: 
-#    for dicionario in DISCIPLINAS: #para cada dicionário que está em disciplina...
-#        disciplina = Disciplina( #cria uma disciplina nova com base no dicionario já existente
-#            id = dicionario['id'],
-#            nome= dicionario['nome'],
-#            descricao= dicionario['descricao']
-#        ) #valores do dicionario são separados 
-#        session.add(disciplina) # deixa disciplina para ser salvo no banco
-#    session.commit()#faz a alteração no banco e adciona os valores
+# with Session(engine) as session: 
+#     for dicionario in DISCIPLINAS: #para cada dicionário que está em disciplina...
+#         disciplina = Disciplina( #cria uma disciplina nova com base no dicionario já existente
+#             id=dicionario['id'],
+#             nome= dicionario['nome'],
+#             descricao= dicionario['descricao']
+#         ) #valores do dicionario são separados 
+#         session.add(disciplina) # deixa disciplina para ser salvo no banco
+#     session.commit()#faz a alteração no banco e adciona os valores
 
-#Fazendo o mesmo com conteúdos...
-#with Session(engine) as session: 
-#    for dicionario in TODOS_OS_CONTEUDOS: 
-#        conteudo = Conteudo(
-#            id = dicionario['id'],
-#            titulo= dicionario['titulo'],
-#            descricao= dicionario['descricao'],
-#            disciplina_id= dicionario['disciplina_id'],
-#        ) 
-#        session.add(conteudo)
-#    session.commit()
+# #Fazendo o mesmo com conteúdos...
+# with Session(engine) as session: 
+#     for dicionario in TODOS_OS_CONTEUDOS: 
+#         conteudo = Conteudo(
+#             id=dicionario['id'],
+#             titulo= dicionario['titulo'],
+#             descricao= dicionario['descricao'],
+#             disciplina_id= dicionario['disciplina_id']
+#         ) 
+#         session.add(conteudo)
+#     session.commit()
+
+# #Fazendo o mesmo com materiais...
+# with Session(engine) as session: 
+#     for dicionario in TODOS_OS_CONTEUDOS:
+#         for material in dicionario['materiais']:
+#             material = Material(
+#                 titulo = material['titulo'],
+#                 url = material['url'],
+#                 conteudo_id=dicionario['id']
+#             )
+#             session.add(material)
+#         session.commit()
--- materiais.py
+++ materiais.py
@@ -1,29 +1,43 @@
 from flask import Blueprint, render_template, redirect, url_for
-from dados_estaticos import buscar_conteudo, buscar_disciplina
+# from dados_estaticos import buscar_conteudo, buscar_disciplina
+from sqlmodel import Session,select
+from database import engine
+from models import Disciplina, Conteudo, Material
 
 materiais_bp = Blueprint('materiais', __name__)
 
 @materiais_bp.route('/materiais/<int:conteudo_id>')
 def ver_materiais(conteudo_id):
-    conteudo_atual = buscar_conteudo(conteudo_id)
+    with Session(engine) as session:
+        busca = select(Conteudo).where(Conteudo.id==conteudo_id)
+        conteudo_atual = session.exec(busca).first()
+        if not conteudo_atual: #se não existir a tal disciplina
+            return "Não encontramos o conteúdo",404
+        busca = select(Disciplina).where(Disciplina.id==conteudo_atual.disciplina_id) 
+        disciplina_atual = session.exec(busca).first()
+        busca = select(Material).where(Material.conteudo_id==conteudo_id)
+        lista_materiais= session.exec(busca).all()
+        print("MATERIAIS ENCONTRADOOOOS:", lista_materiais) 
+        return render_template('materiais.html', disciplina=disciplina_atual, conteudo=conteudo_atual,materiais=lista_materiais)
+    # conteudo_atual = buscar_conteudo(conteudo_id)ss
     
-    if not conteudo_atual:
-        return "Conteúdo não encontrado", 404
+    # if not conteudo_atual:
+    #     return "Conteúdo não encontrado", 404
         
-    disciplina_atual = buscar_disciplina(conteudo_atual['disciplina_id'])
+    # disciplina_atual = buscar_disciplina(conteudo_atual['disciplina_id'])
 
-    return render_template(
-        'materiais.html', disciplina=disciplina_atual, conteudo=conteudo_atual)
+    # return render_template(
+    #     'materiais.html', disciplina=disciplina_atual, conteudo=conteudo_atual)
 
 
-# Atenção! As rotas a seguir estão incompletas e foram adicionadas para evitar erros de redirecionamento
+# # Atenção! As rotas a seguir estão incompletas e foram adicionadas para evitar erros de redirecionamento
 
-# adiciona materiais (Redireciona para atualizar a tela)
-@materiais_bp.route('/adicionar_material/<int:conteudo_id>', methods=['POST'])
-def adicionar_material(conteudo_id):
-    return redirect(url_for('materiais.ver_materiais', conteudo_id=conteudo_id))
+# # adiciona materiais (Redireciona para atualizar a tela)
+# @materiais_bp.route('/adicionar_material/<int:conteudo_id>', methods=['POST'])
+# def adicionar_material(conteudo_id):
+#     return redirect(url_for('materiais.ver_materiais', conteudo_id=conteudo_id))
 
-# exclue materiais (Redireciona para atualizar a tela)
-@materiais_bp.route('/excluir_material/<int:conteudo_id>/<int:index>', methods=['POST'])
-def excluir_material(conteudo_id, index):
-    return redirect(url_for('materiais.ver_materiais', conteudo_id=conteudo_id))
+# # exclue materiais (Redireciona para atualizar a tela)
+# @materiais_bp.route('/excluir_material/<int:conteudo_id>/<int:index>', methods=['POST'])
+# def excluir_material(conteudo_id, index):
+#     return redirect(url_for('materiais.ver_materiais', conteudo_id=conteudo_id))
